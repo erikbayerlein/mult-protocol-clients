b74c61d07a507b1b9f14536497a3e2e064463159
erikbayerlein <erik.bayer@icloud.com>
2025-11-05 21:21:19 -0300

    feat: adding json parser

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,41 +1,52 @@
 package auth
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 
 	"github.com/erikbayerlein/mult-protocol-clients/internal/tcp"
 )
 
-func Auth(alunoID int, host string, port int) (string, error) {
-	authRequest := fmt.Sprintf("AUTH|aluno_id=%d|FIM", alunoID)
-	authResponse, err := tcp.Request(authRequest, host, port)
+type authJSONResp struct {
+	Token string `json:"token"`
+}
+
+var legacyTokenRe = regexp.MustCompile(`token=([^|]*)`)
+
+func Auth(req string, host string, port int) (string, error) {
+	authResponse, err := tcp.Request(req, host, port)
 	if err != nil {
 		return "", err
 	}
-	re := regexp.MustCompile(`token=([^|]*)`)
-	match := re.FindStringSubmatch(authResponse)
-	if len(match) < 2 {
-		return "", fmt.Errorf("token not found in AUTH response")
+
+	fmt.Printf("Received response: %s\n", authResponse)
+
+	var j authJSONResp
+	if err := json.Unmarshal([]byte(authResponse), &j); err == nil && j.Token != "" {
+		return j.Token, nil
 	}
-	return match[1], nil
+
+	if m := legacyTokenRe.FindStringSubmatch(authResponse); len(m) == 2 {
+		return m[1], nil
+	}
+
+	return "", fmt.Errorf("token not found in response")
 }
 
-func RequireLogin() (TokenRecord, error) {
-	rec, err := LoadToken()
-	if err != nil {
-		return TokenRecord{}, fmt.Errorf("no active session. Please 'login <aluno_id>' first")
-	}
-	return rec, nil
-}
-
-func LogoutRemote(token string, host string, port int) error {
-	logoutRequest := fmt.Sprintf("LOGOUT|token=%s|FIM", token)
-
-	_, err := tcp.Request(logoutRequest, host, port)
+func LogoutRemote(req string, host string, port int) error {
+	_, err := tcp.Request(req, host, port)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func RequireLogin() (TokenRecord, error) {
+	rec, err := LoadToken()
+	if err != nil {
+		return TokenRecord{}, fmt.Errorf("No active session. Please 'login <aluno_id>' first")
+	}
+	return rec, nil
 }
