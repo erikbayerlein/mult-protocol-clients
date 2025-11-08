9dd58f7ad26139cd33d33cd36bfb5d6b6572d8b7
erikbayerlein <erik.bayer@icloud.com>
2025-11-08 17:29:36 -0300

    feat: adding request bytes for proto

--- internal/tcp/tcp_request.go
+++ internal/tcp/tcp_request.go
@@ -1,8 +1,11 @@
 package tcp
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
@@ -45,6 +48,58 @@ func Request(message string, host string, port int) (string, error) {
 	response := strings.TrimSpace(string(trimmedData))
 
 	return response, nil
+}
+
+func RequestBytes(payload []byte, host string, port int) ([]byte, error) {
+	address := net.JoinHostPort(host, strconv.Itoa(port))
+
+	if sharedConn == nil || sharedConn.conn == nil {
+		c, err := net.Dial("tcp", address)
+		if err != nil {
+			return nil, fmt.Errorf("connection error: %v", err)
+		}
+		sharedConn = &Connection{conn: c}
+	}
+
+	sharedConn.mu.Lock()
+	defer sharedConn.mu.Unlock()
+
+	w := bufio.NewWriter(sharedConn.conn)
+	r := bufio.NewReader(sharedConn.conn)
+
+	var hdr [4]byte
+	binary.BigEndian.PutUint32(hdr[:], uint32(len(payload)))
+	if _, err := w.Write(hdr[:]); err != nil {
+		return nil, fmt.Errorf("send header error: %v", err)
+	}
+	if _, err := w.Write(payload); err != nil {
+		return nil, fmt.Errorf("send payload error: %v", err)
+	}
+	if err := w.Flush(); err != nil {
+		return nil, fmt.Errorf("flush error: %v", err)
+	}
+
+	if _, err := io.ReadFull(r, hdr[:]); err != nil {
+		if !strings.Contains(err.Error(), "EOF") {
+			return nil, fmt.Errorf("read header error: %v", err)
+		}
+		return nil, err
+	}
+
+	n := binary.BigEndian.Uint32(hdr[:])
+	if n == 0 {
+		return []byte{}, nil
+	}
+
+	resp := make([]byte, n)
+	if _, err := io.ReadFull(r, resp); err != nil {
+		if !strings.Contains(err.Error(), "EOF") {
+			return nil, fmt.Errorf("read body error: %v", err)
+		}
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 func Close() error {
